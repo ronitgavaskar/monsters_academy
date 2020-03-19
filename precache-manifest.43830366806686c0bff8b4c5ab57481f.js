630f3b27a2b853623bb3e19b6529c0b097acc7c6
ronitgavaskar <ronitgavaskar@gmail.com>
2020-03-19 11:51:44 -0700

    Updates

---
+++ precache-manifest.43830366806686c0bff8b4c5ab57481f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6aa5d7f8505ff36ed7c9b5dfdd17ab6",
+    "url": "/monsters_academy/index.html"
+  },
+  {
+    "revision": "8e7f839309dacfedcec5",
+    "url": "/monsters_academy/static/css/main.d32b8002.chunk.css"
+  },
+  {
+    "revision": "ae70c1ff0b495ba8c91d",
+    "url": "/monsters_academy/static/js/2.c72f5ba6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters_academy/static/js/2.c72f5ba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e7f839309dacfedcec5",
+    "url": "/monsters_academy/static/js/main.b9fe0636.chunk.js"
+  },
+  {
+    "revision": "61ff3610aed1b240a23b",
+    "url": "/monsters_academy/static/js/runtime-main.975c514c.js"
+  }
+]);

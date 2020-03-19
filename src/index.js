cf765e9ebfa5fffb84bf6b7b78269b4fe24380f7
ronitgavaskar <ronitgavaskar@gmail.com>
2020-03-18 21:19:21 -0700

    monsters card push

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//replaces whatever element has Id root with our function that returns some html
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
